6bf1e98e7084dba47ae7bc2f50b398ab2cf19c01
Vladimir Yugay <yugay@magazino.eu>
2020-05-30 09:08:01 +0200

    Dragon and surface implemented

--- exercise_04/dragon.cpp
+++ exercise_04/dragon.cpp
@@ -5,11 +5,35 @@
 #include <math.h>
 
 
-// TODO: Implement the cost function
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////Vladimir Yugay//////////////////////////////////////////
+////////////////////////Jan Ruttinger///////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
 struct RegistrationCostFunction
 {
+	RegistrationCostFunction(const Point2D& source_, const Point2D& target_, const Weight& w_)
+		: source(source_), w(w_), target(target_)
+	{
+	}
 
+	template<typename T>
+	bool operator()(const T* const u, const T* const v, const T* const theta, T* residual) const
+	{
+		// TODO: Implement the cost function
+		T a = cos(theta[0])* source.x - sin(theta[0])* source.y + u[0] - target.x;
+		T b = sin(theta[0])* source.x + cos(theta[0])* source.y + v[0] - target.y;
+		residual[0] = w.w * (pow(a, 2) + pow(b, 2));
+		return true;
+	}
+
+private:
+	const Point2D source;
+	const Point2D target;
+	const Weight w;
 };
+
 
 
 int main(int argc, char** argv)
@@ -17,15 +41,27 @@ int main(int argc, char** argv)
 	google::InitGoogleLogging(argv[0]);
 
 	// TODO: Read data points and the weights. Define the parameters of the problem
-	const std::string file_path_1 = "../data/points_dragon_1.txt";
-	const std::string file_path_2 = "../data/points_dragon_2.txt";
-	const std::string file_path_weights = "../data/weights_dragon.txt";
+	const std::string file_path_1 = "../../data/exercise_4_data/points_dragon_1.txt";
+	const std::string file_path_2 = "../../data/exercise_4_data/points_dragon_2.txt";
+	const std::string file_path_weights = "../../data/exercise_4_data/weights_dragon.txt";
 
+	const auto source = read_points_from_file<Point2D>(file_path_1);
+	const auto target = read_points_from_file<Point2D>(file_path_2);
+	const auto weights = read_points_from_file<Weight>(file_path_weights);
+
+	double theta = 0.0;
+	double u = 0.0;
+	double v = 0.0;
 
 	ceres::Problem problem;
 
-	// TODO: For each weighted correspondence create one residual block
-
+	for (size_t i = 0; i < source.size(); ++i) {
+		problem.AddResidualBlock(
+			new ceres::AutoDiffCostFunction<RegistrationCostFunction, 1, 1, 1, 1> (
+				new RegistrationCostFunction(source[i], target[i], weights[i])), 
+			nullptr, &u, &v, &theta
+		); 
+	}
 
 	ceres::Solver::Options options;
 	options.max_num_iterations = 25;
@@ -36,8 +72,7 @@ int main(int argc, char** argv)
 	ceres::Solve(options, &problem, &summary);
 
 	std::cout << summary.BriefReport() << std::endl;
-
-	// TODO: Output the final values of the translation and rotation (in degree)
+	std::cout << "Final theta: " << theta*180.0/3.14 << "\tu: " << u << "\tv: " << v << std::endl;
 
 	system("pause");
 	return 0;
--- exercise_04/surface.cpp
+++ exercise_04/surface.cpp
@@ -8,7 +8,20 @@
 // TODO: Implement the cost function
 struct SurfaceCostFunction
 {
+	SurfaceCostFunction(const Point3D& point_): point(point_)
+	{
+	}
 
+	template<typename T>
+	bool operator()(const T* const a, const T* const b, const T* const c, T* residual) const
+	{	
+		residual[0] = point.z - (
+			pow(point.x, 2) / a[0] - pow(point.y, 2) / b[0]) / c[0];
+		return true;
+	}
+
+private:
+	const Point3D point;
 };
 
 
@@ -17,12 +30,22 @@ int main(int argc, char** argv)
 	google::InitGoogleLogging(argv[0]);
 
 	// TODO: Read 3D surface data points and define the parameters of the problem
-	const std::string file_path = "../data/points_surface.txt";
-
+	const std::string file_path = "../../data/exercise_4_data/points_surface.txt";
+	const auto points = read_points_from_file<Point3D>(file_path);
 	ceres::Problem problem;
 
 	// TODO: For each data point create one residual block
+	const double a_initial = 1.0, b_initial = 1.0, c_initial = 1.0;
+	double a = a_initial, b = b_initial, c = c_initial; 
 
+	for (auto& point: points)
+	{
+		problem.AddResidualBlock(
+			new ceres::AutoDiffCostFunction<SurfaceCostFunction, 1, 1, 1, 1>(
+				new SurfaceCostFunction(point)),
+			nullptr, &a, &b, &c 
+		);
+	}
 
 	ceres::Solver::Options options;
 	options.max_num_iterations = 100;
@@ -35,6 +58,8 @@ int main(int argc, char** argv)
 	std::cout << summary.BriefReport() << std::endl;
 	
 	// TODO: Output the final values of the parameters
+	std::cout << "Initial a: " << a_initial << "\tb: " << b_initial << "\tc" << c_initial << std::endl;
+	std::cout << "Final a: " << a << "\tb: " << b << "\tc: " << c << std::endl;
 
 
 	system("pause");
